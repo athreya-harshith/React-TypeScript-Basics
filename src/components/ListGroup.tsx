1f777fcd3c6bcc3589afbc0e8251b0206477b6e1
athreya-harshith <athreya1207@gmail.com>
2024-02-17 08:52:19 +0530

    Event Handling

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,7 +1,12 @@
 // import { Fragment } from "react";
+import { MouseEvent } from "react";
 function ListGroup() {
   let items = ["New York", "Virginia", "San Fransico", "Florida"];
   //   items = [];
+  const handleClick = (event: MouseEvent) => {
+    console.log("Clicked the list item ");
+    console.log(event);
+  };
   return (
     <>
       <h1>List Group</h1>
@@ -9,7 +14,7 @@ function ListGroup() {
         {/* the below is the better way for conditional rendering */}
         {items.length === 0 && <p>No Items Found</p>}
         {items.map((item) => (
-          <li key={item} className="list-group-item">
+          <li key={item} className="list-group-item" onClick={handleClick}>
             {item}
           </li>
         ))}
